eca50a5cdcba61e7d621ae8e63b45b98480d09be
Roee Getz <roeegetz@Roees-MacBook-Pro.local>
2025-04-26 12:05:47 -0700

    first commit

---
+++ suger_reading.py
@@ -0,0 +1,32 @@
+from pydexcom import Dexcom
+import time
+
+LOW_SUGER = 60
+HIGH_SUGER = 250
+x=0
+suger_dict = {}
+
+flag = True
+dexcom = Dexcom(username='roee.dexcom',password='Sdfwer234',region="ous")
+glucose = dexcom.get_current_glucose_reading()
+
+
+
+
+def HIGH_or_LOW(glucose_reading):
+    if glucose_reading <= LOW_SUGER:
+        return "low"
+    elif glucose_reading >= HIGH_SUGER:
+        return "high"
+    else:
+        return "ok"
+    
+
+
+while flag:
+    
+    glucose = dexcom.get_current_glucose_reading()
+    print(f"Glucose: {glucose.value} mg/dL is {HIGH_or_LOW(glucose.value)}") 
+    suger_dict["Glucose"] = glucose.value
+    time.sleep(250)
+
