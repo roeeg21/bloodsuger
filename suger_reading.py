c3fa3c3a675f920fd82dba1779df31dcae5622e7
Roee Getz <roeegetz@Roees-MacBook-Pro.local>
2025-05-29 11:02:55 -0700

    adding env vars and change the code

--- suger_reading.py
+++ suger_reading.py
@@ -1,13 +1,15 @@
 from pydexcom import Dexcom
 import time
+import os
 
 LOW_SUGER = 60
 HIGH_SUGER = 250
-x=0
+USERNAME = os.getenv('Dexcom_username')
+PASSWORD = os.getenv('Dexcom_password')
 suger_dict = {}
 
 flag = True
-dexcom = Dexcom(username='roee.dexcom',password='Sdfwer234',region="ous")
+dexcom = Dexcom(username=USERNAME,password=PASSWORD,region="ous")
 glucose = dexcom.get_current_glucose_reading()
 
 
@@ -33,4 +35,14 @@ def get_glucose_reading():
         }
         return suger_dict
     except Exception as e:
-        return {"error": str(e)}
+        glucose = dexcom.get_latest_glucose_reading()
+        suger_dict = {
+            "Glucose": glucose.value,
+            "Status": HIGH_or_LOW(glucose.value),
+            "Trend": glucose.trend_description,
+            "Time": glucose.datetime.strftime("%Y-%m-%d %H:%M:%S")
+        }
+        return suger_dict
+    
+
+print(get_glucose_reading())
