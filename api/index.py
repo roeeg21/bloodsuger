7494b080b69cd322995f1bc33692262755564dd9
Roee Getz <roeegetz@Roees-MacBook-Pro.local>
2026-01-16 13:19:43 +0200

    try5

--- api/index.py
+++ api/index.py
@@ -16,9 +16,7 @@ def get_glucose_reading():
     try:
         # Using region="ous" for outside US as per your requirement
         #dexcom = Dexcom(username=USERNAME, password=PASSWORD, ous=True)
-        dexcom = Dexcom(username="roee.dexcom",password="Sdfwer234", ous=True)
-        if Exception :
-            dexcom = Dexcom(username="roee.dexcom",password="Sdfwer234", us=True)
+        dexcom = Dexcom(username="roee.dexcom",password="Sdfwer234", region="us")
         glucose_reading = dexcom.get_current_glucose_reading()
         
         
